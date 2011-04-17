5bd7eabc6a60200f17bfa417120a2f43fc0a85a7
bmuschko <benjamin.muschko@gmail.com>
2011-04-17 10:32:22 -0400

    Fixing tabs.

--- src/main/groovy/org/gradle/api/plugins/gaelyk/tools/PluginManager.groovy
+++ src/main/groovy/org/gradle/api/plugins/gaelyk/tools/PluginManager.groovy
@@ -34,195 +34,195 @@ import org.gradle.api.logging.Logging
  */
 class PluginManager {
     static final Logger LOGGER = Logging.getLogger(PluginManager.class)
-	static final String GAELYKHISTORY = ".gaelykhistory"
-	static final String GAELYKPLUGIN = ".gaelykplugin"
-	final File projectRoot
-	
-	PluginManager(projectRoot = "."){
-		this.projectRoot = projectRoot as File
-	}
-	
-	def install(plugin){
-		switch (plugin) {
-		case ~/http[s]?:.*?.zip/:
-			installFromUrl plugin
-			break
-		case ~/.*?\.zip/:
-			installFromZip plugin as File
-			break
-		default:
-			LOGGER.error "Cannot install plugin: $plugin"
-		}
-	}
-	
-	def getHistory() {
-		new XmlSlurper().parse(historyFile)
-	}
-	
-	
-	def uninstall(origin){
-		filesToDelete(origin).each{
-			def theFile = new File(projectRoot, it)
-			if(it =~ "war/WEB-INF/plugins/\\w+.groovy") {
-				def name = theFile.name - ".groovy"
-				def plugins = new File(projectRoot, "war/WEB-INF/plugins.groovy")
-				if(plugins.exists()){
-					if(plugins.readLines().grep(~/\s*install\s+$name\s*/)){
-						plugins.text = plugins.text.replaceAll(/\s*install\s+$name\s*/, "")
-					}
-					
-					if(!plugins.text.trim()){
-						plugins.delete()
-					}
-				}
-			}
-			if(theFile.exists()){
-				theFile.deleteOnExit()
-			}
-		}
-		removeHistoryRecord origin
-	}
-	
-	private installFromUrl(url){
-		File tmp = File.createTempFile("gaelyk-plugin-download-" + new Random().nextInt(), ".zip")
-		new AntBuilder().get(src: url, dest: tmp.path)
-		installFromZip tmp, url
-		tmp.delete()
-	}
-	
-	private installFromZip(File zip, origin = zip.path){
-		TempDir.withTempDir 'gaelyk-unzipped-plugin', { tmp ->
-			new AntBuilder().unzip src: zip.path, dest: tmp.path
-			
-			installFromDir(tmp, origin)
+    static final String GAELYKHISTORY = ".gaelykhistory"
+    static final String GAELYKPLUGIN = ".gaelykplugin"
+    final File projectRoot
 
-		}
-	}
-	
-	private installFromDir(tmp, origin = tmp){
-		def props = readPluginDescriptor(tmp.path)
-		
-		TempDir.withTempDir 'gaelyk-plugin-stage', { stage ->
-			
-			new AntBuilder().copy(todir: stage.path){
-				fileset(dir: tmp.path){
-					exclude(name: "*build.*")
-					exclude(name: ".**")
-					exclude(name: "**/.*/**")
-					for(ex in props.excludes){
-						exclude(name: ex)
-					}
-				}
-				if(props.includes){
-					fileset(dir: tmp.path){
-						for(ex in props.includes){
-							include(name: ex)
-						}
-					}					
-				}
-			}
-			
-			addHistory(stage, origin)
-			installPlugins(stage)
-			
-			new AntBuilder().copy(todir: projectRoot.path){
-				fileset(dir: stage.path)
-			}
-		}
-	}
-	
-	private readPluginDescriptor(pluginRoot){
-		def file = new File(pluginRoot as File, GAELYKPLUGIN) 
-		if(!file.exists()){
-			return []
-		}
+    PluginManager(projectRoot = "."){
+        this.projectRoot = projectRoot as File
+    }
+
+    def install(plugin){
+        switch (plugin) {
+        case ~/http[s]?:.*?.zip/:
+            installFromUrl plugin
+            break
+        case ~/.*?\.zip/:
+            installFromZip plugin as File
+            break
+        default:
+            LOGGER.error "Cannot install plugin: $plugin"
+        }
+    }
+
+    def getHistory() {
+        new XmlSlurper().parse(historyFile)
+    }
+
+
+    def uninstall(origin){
+        filesToDelete(origin).each{
+            def theFile = new File(projectRoot, it)
+            if(it =~ "war/WEB-INF/plugins/\\w+.groovy") {
+                def name = theFile.name - ".groovy"
+                def plugins = new File(projectRoot, "war/WEB-INF/plugins.groovy")
+                if(plugins.exists()){
+                    if(plugins.readLines().grep(~/\s*install\s+$name\s*/)){
+                        plugins.text = plugins.text.replaceAll(/\s*install\s+$name\s*/, "")
+                    }
+
+                    if(!plugins.text.trim()){
+                        plugins.delete()
+                    }
+                }
+            }
+            if(theFile.exists()){
+                theFile.deleteOnExit()
+            }
+        }
+        removeHistoryRecord origin
+    }
+
+    private installFromUrl(url){
+        File tmp = File.createTempFile("gaelyk-plugin-download-" + new Random().nextInt(), ".zip")
+        new AntBuilder().get(src: url, dest: tmp.path)
+        installFromZip tmp, url
+        tmp.delete()
+    }
+
+    private installFromZip(File zip, origin = zip.path){
+        TempDir.withTempDir 'gaelyk-unzipped-plugin', { tmp ->
+            new AntBuilder().unzip src: zip.path, dest: tmp.path
+
+            installFromDir(tmp, origin)
+
+        }
+    }
+
+    private installFromDir(tmp, origin = tmp){
+        def props = readPluginDescriptor(tmp.path)
+
+        TempDir.withTempDir 'gaelyk-plugin-stage', { stage ->
+
+            new AntBuilder().copy(todir: stage.path){
+                fileset(dir: tmp.path){
+                    exclude(name: "*build.*")
+                    exclude(name: ".**")
+                    exclude(name: "**/.*/**")
+                    for(ex in props.excludes){
+                        exclude(name: ex)
+                    }
+                }
+                if(props.includes){
+                    fileset(dir: tmp.path){
+                        for(ex in props.includes){
+                            include(name: ex)
+                        }
+                    }
+                }
+            }
+
+            addHistory(stage, origin)
+            installPlugins(stage)
+
+            new AntBuilder().copy(todir: projectRoot.path){
+                fileset(dir: stage.path)
+            }
+        }
+    }
+
+    private readPluginDescriptor(pluginRoot){
+        def file = new File(pluginRoot as File, GAELYKPLUGIN)
+        if(!file.exists()){
+            return []
+        }
         Binding binding = []
-		GroovyShell shell = [binding]
-		shell.evaluate file.text
-		binding.variables
-	}
-	
-	private readHistory(){
-		def historyFile = new File(projectRoot, GAELYKHISTORY) 
-		
-	}
-	
-	private addHistory(stage, pluginUrl){
-		def theHistory = history
-		def pluginHistory = theHistory.plugin.find{it.@origin == pluginUrl}
-		if(pluginHistory){
-			pluginHistory.replaceNode{
-				plugin(origin: pluginUrl, installed: new Date(), name: getBareName(pluginUrl.toString())){
-					stage.eachFileRecurse{ theFile ->
-						file(name: theFile.path[stage.path.size()..-1])
-					}
-				}
-			}
-		} else {
-			theHistory.appendNode{
-				plugin(origin: pluginUrl, installed: new Date(), name: getBareName(pluginUrl.toString())){
-					stage.eachFileRecurse{ theFile ->
-						file(name: theFile.path[stage.path.size()..-1])
-					}
-				}
-			}
-		}
-		writeHistory(theHistory)
-	}
-	
-	private getBareName(String origin){
-		origin[(origin.lastIndexOf("/")+1)..(origin.lastIndexOf(".")-1)]
-	}
+        GroovyShell shell = [binding]
+        shell.evaluate file.text
+        binding.variables
+    }
 
-	private writeHistory(history) {
-		historyFile.text = new StreamingMarkupBuilder().bind{ mkp.yield history }
-	}
+    private readHistory(){
+        def historyFile = new File(projectRoot, GAELYKHISTORY)
 
-	private File getHistoryFile() {
-		def historyFile = new File(projectRoot, GAELYKHISTORY)
-		if(!historyFile.exists()){
-			historyFile.createNewFile()
-			historyFile.append("<history></history>")
-		}
-		return historyFile
-	}
-	
-	private filesToDelete(origin){
-		def installedBy = getHistoryPlugin(origin).file*.@name*.text()
-		def byOthers = []
-		history.plugin.findAll{it.@origin != origin && it.@name != origin }.each{
-			 byOthers << it.file.@name.text()
-		}
-		installedBy - byOthers
-	}
-	
-	private getHistoryPlugin(origin, theHistory = history){
-		theHistory.plugin.find{it.@origin == origin || it.@name == origin }
-	}
-	
-	private removeHistoryRecord(origin){
-		def history = history
-		getHistoryPlugin(origin, history).replaceNode{}
-		writeHistory history
-	}
-	
-	private installPlugins(origin){
-		def pluginsDir = new File(origin as File, "/war/WEB-INF/plugins")
-		if (pluginsDir.exists()) {
-			def plugins = new File("$projectRoot.path/war/WEB-INF/plugins.groovy")
-			if(!plugins.exists()){
-				new AntBuilder().sequential{ mkdir(dir: "$projectRoot.path/war/WEB-INF") }
-				assert plugins.createNewFile()
-			}
-			pluginsDir.list().each {
-				def pluginName = it - ".groovy"
-				if(!plugins.readLines().grep(~/\s*install\s+$pluginName\s*/))
-				plugins.append("\ninstall $pluginName\n")
-			}
-		}
-	}
-	
-	private getInstalledPlugins(){
-		history.plugin.@origin*.text()
-	}
+    }
+
+    private addHistory(stage, pluginUrl){
+        def theHistory = history
+        def pluginHistory = theHistory.plugin.find{it.@origin == pluginUrl}
+        if(pluginHistory){
+            pluginHistory.replaceNode{
+                plugin(origin: pluginUrl, installed: new Date(), name: getBareName(pluginUrl.toString())){
+                    stage.eachFileRecurse{ theFile ->
+                        file(name: theFile.path[stage.path.size()..-1])
+                    }
+                }
+            }
+        } else {
+            theHistory.appendNode{
+                plugin(origin: pluginUrl, installed: new Date(), name: getBareName(pluginUrl.toString())){
+                    stage.eachFileRecurse{ theFile ->
+                        file(name: theFile.path[stage.path.size()..-1])
+                    }
+                }
+            }
+        }
+        writeHistory(theHistory)
+    }
+
+    private getBareName(String origin){
+        origin[(origin.lastIndexOf("/")+1)..(origin.lastIndexOf(".")-1)]
+    }
+
+    private writeHistory(history) {
+        historyFile.text = new StreamingMarkupBuilder().bind{ mkp.yield history }
+    }
+
+    private File getHistoryFile() {
+        def historyFile = new File(projectRoot, GAELYKHISTORY)
+        if(!historyFile.exists()){
+            historyFile.createNewFile()
+            historyFile.append("<history></history>")
+        }
+        return historyFile
+    }
+
+    private filesToDelete(origin){
+        def installedBy = getHistoryPlugin(origin).file*.@name*.text()
+        def byOthers = []
+        history.plugin.findAll{it.@origin != origin && it.@name != origin }.each{
+             byOthers << it.file.@name.text()
+        }
+        installedBy - byOthers
+    }
+
+    private getHistoryPlugin(origin, theHistory = history){
+        theHistory.plugin.find{it.@origin == origin || it.@name == origin }
+    }
+
+    private removeHistoryRecord(origin){
+        def history = history
+        getHistoryPlugin(origin, history).replaceNode{}
+        writeHistory history
+    }
+
+    private installPlugins(origin){
+        def pluginsDir = new File(origin as File, "/war/WEB-INF/plugins")
+        if (pluginsDir.exists()) {
+            def plugins = new File("$projectRoot.path/war/WEB-INF/plugins.groovy")
+            if(!plugins.exists()){
+                new AntBuilder().sequential{ mkdir(dir: "$projectRoot.path/war/WEB-INF") }
+                assert plugins.createNewFile()
+            }
+            pluginsDir.list().each {
+                def pluginName = it - ".groovy"
+                if(!plugins.readLines().grep(~/\s*install\s+$pluginName\s*/))
+                plugins.append("\ninstall $pluginName\n")
+            }
+        }
+    }
+
+    private getInstalledPlugins(){
+        history.plugin.@origin*.text()
+    }
 }
--- src/main/groovy/org/gradle/api/plugins/gaelyk/tools/TempDir.groovy
+++ src/main/groovy/org/gradle/api/plugins/gaelyk/tools/TempDir.groovy
@@ -21,27 +21,26 @@ package org.gradle.api.plugins.gaelyk.tools
  * @author Vladimir Orany
  */
 class TempDir {
-	@Lazy static tempDir = new File(System.getProperty('java.io.tmpdir'))
-	
-	static File createNew(prefix, suffix = new Random().nextInt()){
-		File where = findDir(prefix, suffix)
-		if(!where.exists()) {
-			where.mkdir()
-		}
-		where
-	}
-	
-	static File findDir(prefix, suffix){
-		new File(tempDir, "$prefix-$suffix")
-	}
-	
-	static withTempDir(prefix, suffix = new Random().nextInt(), Closure closure){
-		File where = findDir(prefix, suffix)
-		try{
-			closure.call(where)
-		} finally {
-			new AntBuilder().delete dir: where.path
-		}
-	}
-	
+    @Lazy static tempDir = new File(System.getProperty('java.io.tmpdir'))
+
+    static File createNew(prefix, suffix = new Random().nextInt()){
+        File where = findDir(prefix, suffix)
+        if(!where.exists()) {
+            where.mkdir()
+        }
+        where
+    }
+
+    static File findDir(prefix, suffix){
+        new File(tempDir, "$prefix-$suffix")
+    }
+
+    static withTempDir(prefix, suffix = new Random().nextInt(), Closure closure){
+        File where = findDir(prefix, suffix)
+        try{
+            closure.call(where)
+        } finally {
+            new AntBuilder().delete dir: where.path
+        }
+    }
 }
