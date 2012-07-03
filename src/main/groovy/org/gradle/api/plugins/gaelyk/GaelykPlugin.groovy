a66669942cfa927d07b5bfb7c887ffe2ed44a101
Marcin Erdmann <erdi84@gmail.com>
2012-07-03 20:59:28 +0100

    Removed unused import

--- src/main/groovy/org/gradle/api/plugins/gaelyk/GaelykPlugin.groovy
+++ src/main/groovy/org/gradle/api/plugins/gaelyk/GaelykPlugin.groovy
@@ -17,7 +17,6 @@ package org.gradle.api.plugins.gaelyk
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.api.file.FileCollection
 import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.plugins.JavaPluginConvention
